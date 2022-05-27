1118b356fd6a49808c299000ee61391002602b59
G. Da Graca <guillaume.dagraca@gmail.com>
2022-05-27 15:53:20 +0200

    review linux VM configuration

---
+++ Linux/provisions/scripts/install-tools.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# install kdiff3
+sudo apt-get install -y kdiff3 libnss3-tools curl gnome-terminal wmctrl gitk pdfcube software-properties-common apt-transport-https wget jq
+
+# Install postman
+#sudo snap install postman
+
+# 
